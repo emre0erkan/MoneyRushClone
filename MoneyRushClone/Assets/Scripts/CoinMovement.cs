89e46ec471f61518d598a197969da95d16196c32
Emre Erkan <93214106+emre0erkan@users.noreply.github.com>
2022-04-18 15:28:21 +0300

    Gates Added

--- MoneyRushClone/Assets/Scripts/CoinMovement.cs
+++ MoneyRushClone/Assets/Scripts/CoinMovement.cs
@@ -5,15 +5,23 @@ using UnityEngine;
 public class CoinMovement : MonoBehaviour
 {
     public static int rotateCoin = 2;
+    [SerializeField] float swerveSpeed;
+    [SerializeField] float maxSwerveAmount;
 
-    private void Start()
+    SwerveInputSystem swerveInput;
+
+    private void Awake()
     {
+        swerveInput = GetComponent<SwerveInputSystem>();
     }
 
-    // Update is called once per frame
     void Update()
     {
         transform.Rotate(Vector3.left * rotateCoin);
         transform.position = transform.position + new Vector3(0, 0, 0.1f);
+        Vector3 sideMove = -transform.right * swerveSpeed * swerveInput.MoveFactorX * Time.deltaTime;
+        transform.position = transform.position + sideMove;
+
+        transform.position = new Vector3(Mathf.Clamp(transform.position.x, -2.3f, 2.3f), transform.position.y, transform.position.z);
     }
 }
