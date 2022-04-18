0e18eb248c79f8e469c2055f504d6c30b393e455
Emre Erkan <93214106+emre0erkan@users.noreply.github.com>
2022-04-18 19:04:06 +0300

    fixing instantiate

--- MoneyRushClone/Assets/Scripts/SpawnCoins.cs
+++ MoneyRushClone/Assets/Scripts/SpawnCoins.cs
@@ -4,21 +4,27 @@ using UnityEngine;
 
 public class SpawnCoins : MonoBehaviour
 {
-    float cashAmount = 0.5f;
+    //float cashAmount = 0.5f;
+    float newCoinDistance = 0.02f;
     
 
-    [SerializeField] GameObject mainCoin;
+    
     [SerializeField] GameObject newCoin;
 
     private void SpawnCoin()
     {
-        Quaternion newCoinRotate = Quaternion.Euler(0, 0, 90);
-        Vector3 nextSpawnPoint = new Vector3(0, -4.22f, transform.position.z + 0.005f);
-        newCoin = Instantiate(newCoin, nextSpawnPoint, newCoinRotate);
-        newCoin.transform.SetParent(mainCoin.transform, true);
+        newCoin = Instantiate(newCoin);
+        newCoin.transform.parent = transform;
+        newCoin.transform.localPosition = new Vector3(transform.position.x + newCoinDistance, 0, 0);
+        newCoinDistance++;
+        newCoin.transform.rotation = Quaternion.Euler(0, 0, 90);
+        //Quaternion newCoinRotate = Quaternion.Euler(0, 0, 90);
+        //Vector3 nextSpawnPoint = new Vector3(transform.position.x + newCoinDistance, -4.22f, 0);
+        //newCoin = Instantiate(newCoin, nextSpawnPoint, newCoinRotate);
+
     }
 
-    private void OnTriggerEnter(Collider other)
+    private void OnTriggerExit(Collider other)
     {
         if (other.gameObject.tag == "BlueGate")
         {
